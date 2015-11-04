f35dfcfbf17fd211d8b9d64169ee0e4208e644b9
Anton Iškov <iskov.anton@yandex.com>
2015-11-04 21:26:21 +0100

    Update filter.py

--- filter.py
+++ filter.py
@@ -1,33 +1,7 @@
 from openpyxl import load_workbook, Workbook
-import re 
 import utils
-import sys
+from settings import *
 
-# Откуда копировать
-origin_filename = 'test.xlsx'
-origin_sheetname = 'Arkusz1'
-
-# Куда копировать
-target_filename = 'result.xlsx'
-target_sheetname = 'Sheet1'
-
-def row_condition(ws, row):
-	return True # re.compile(r".*(FCU).*").match(ws['B%s' % row].value)
- 
-# Интервал строк для копирования
-rows_range = range(2, 45238)
-# Колонки которые мы хотим скопировать
-columns = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'[0:15]
-
-# Колонка по которой группируем
-group_by = 'A'
-
-# Условие выбор группы
-def group_condition(ws, rows):
-	# Указываем utils.sum_by_group(ws, rows, <>, <>)
-	return utils.is_represented(ws, rows, 'O', 'ZIMA 2012')
-	
-############################################################################
 print('Source file opening')
 
 # Open origin
@@ -52,7 +26,6 @@ else:
 
 	print('Starting grouping')
 	for row in rows_range:
-		sys.stdout.write('.')
 		group_value = ws_origin['%s%s' % (group_by, row)].value
 
 		if group_value not in groups:
@@ -65,7 +38,6 @@ else:
 	# Check group condition
 	print('Starting group filtering')
 	for key, rows in groups.iteritems():
-		sys.stdout.write('.')
 		if group_condition(ws_origin, rows):
 			selected_groups.append(key)
 
@@ -76,7 +48,6 @@ target_row = 1
 
 print('Start copying')
 for row in rows_range:
-	sys.stdout.write('.')
 	check = not row_condition or row_condition(ws_origin, row)
 	check = check and (not group_condition or ws_origin['%s%s' % (group_by, row)].value in selected_groups)
 
