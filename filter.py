44a05c791a169f9d66370b7333bad0fb66401cdd
Anton Iškov <iskov.anton@yandex.com>
2015-11-08 14:18:01 +0100

    Update filter.py

--- filter.py
+++ filter.py
@@ -13,69 +13,69 @@ print('Source file opened')
 
 # Check groups condition in case if exists
 if not group_condition:
-	selected_groups = None
-else: 
-	selected_groups = []
+    selected_groups = None
+else:
+    selected_groups = []
 
-	# Group rows 
-	groups = dict()
+    # Group rows 
+    groups = dict()
 
-	print('Starting grouping')
-	for row in rows_range:
+    print('Starting grouping')
+    for row in rows_range:
         group_value = ws_origin['%s%s' % (group_by, row)].value
 
-		if group_value not in groups:
-			groups[group_value] = []
+    if group_value not in groups:
+        groups[group_value] = []
 
-		groups[group_value].append(row)
+    groups[group_value].append(row)
 
-	print('File grouped to %s by column %s.' % (len(groups), group_by))
+print('File grouped to %s by column %s.' % (len(groups), group_by))
 
-	# Check group condition
-	print('Starting group filtering')
-	for key, rows in groups.iteritems():
-		if group_condition(ws_origin, rows):
-			selected_groups.append(key)
+# Check group condition
+print('Starting group filtering')
+for key, rows in groups.iteritems():
+    if group_condition(ws_origin, rows):
+        selected_groups.append(key)
 
-	print('%s groups found that match the conditions.' % len(selected_groups))
+print('%s groups found that match the conditions.' % len(selected_groups))
 
 # Copy 
 if target_filename.endswith('xlsx'):
-	target_row = 1
-	
-	# Open target
-	wb_target = Workbook()
-	ws_target = wb_target.active
-	ws_target.title = target_sheetname
+    target_row = 1
 
-	print('Start copying')
-	for row in rows_range:
-		check = not row_condition or row_condition(ws_origin, row)
-		check = check and (not group_condition or ws_origin['%s%s' % (group_by, row)].value in selected_groups)
-	
-		if check:
-			for column in columns:
-				ws_target['%s%s' % (column, target_row)] = ws_origin['%s%s' % (column, row)].value
-			
-			target_row += 1
+    # Open target
+    wb_target = Workbook()
+    ws_target = wb_target.active
+    ws_target.title = target_sheetname
 
-	# Save target file
-	wb_target.save(filename = target_filename)
+    print('Start copying')
+    for row in rows_range:
+        check = not row_condition or row_condition(ws_origin, row)
+        check = check and (not group_condition or ws_origin['%s%s' % (group_by, row)].value in selected_groups)
+
+        if check:
+            for column in columns:
+                ws_target['%s%s' % (column, target_row)] = ws_origin['%s%s' % (column, row)].value
+
+            target_row += 1
+
+    # Save target file
+    wb_target.save(filename = target_filename)
 else:
-	with open(target_filename, 'wb') as f:
-	    wb_target = csv.writer(f)
-	    
-    	print('Start copying')
-    	for row in rows_range:
-    		check = not row_condition or row_condition(ws_origin, row)
-    		check = check and (not group_condition or ws_origin['%s%s' % (group_by, row)].value in selected_groups)
-    	
-    		if check:
-    			row = []
-    			for column in columns:
-    				row.append(ws_origin['%s%s' % (column, row)].value)
-    			
-    		    wb_target.writerow(row)
+    with open(target_filename, 'wb') as f:
+        wb_target = csv.writer(f)
+
+    print('Start copying')
+    for row in rows_range:
+        check = not row_condition or row_condition(ws_origin, row)
+        check = check and (not group_condition or ws_origin['%s%s' % (group_by, row)].value in selected_groups)
+
+        if check:
+            row = []
+            for column in columns:
+                row.append(ws_origin['%s%s' % (column, row)].value)
+
+        wb_target.writerow(row)
 
 
 # Print report and wait input
